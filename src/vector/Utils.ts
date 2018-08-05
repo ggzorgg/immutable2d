a60b942625fb193b571ea442646f312d62496547
ggzorgg <41978414+ggzorgg@users.noreply.github.com>
2018-08-04 22:43:36 -0500

    Adding getComponents function

--- src/vector/Utils.ts
+++ src/vector/Utils.ts
@@ -1,6 +1,15 @@
+import { VectorLike } from '../Types'
 import { VectorObject } from './VectorObject'
 
 export function isVectorObject(value: any): value is VectorObject {
   const vector = value as VectorObject
   return vector.x !== undefined && vector.y !== undefined
+}
+
+export function getComponents(value: VectorLike): [number, number] {
+  if (isVectorObject(value)) {
+    return [value.x, value.y]
+  } else {
+    return value
+  }
 }
--- test/vector/utils.spec.ts
+++ test/vector/utils.spec.ts
@@ -1,14 +1,28 @@
 import { expect } from 'chai'
 import 'mocha'
-import { isVectorObject } from '../../src/vector/utils'
+import { getComponents, isVectorObject } from '../../src/vector/utils'
 
 describe('The "isVectorObject" function', () => {
   it('should return true when object has both x and y properties', () => {
     const obj = { x: 10, y: 20, z: 30 }
     expect(isVectorObject(obj)).to.be.true
   }),
-  it('should return false when object doesn´t have both x and y properties', () => {
-    const obj = { x: 10, z: 40 }
-    expect(isVectorObject(obj)).to.be.false
-  })
+    it('should return false when object doesn´t have both x and y properties', () => {
+      const obj = { x: 10, z: 40 }
+      expect(isVectorObject(obj)).to.be.false
+    })
+})
+
+describe('The "getComponents" function', () => {
+  it('should return the tuple when the parameter is a tuple', () => {
+    const obj: [number, number] = [10, 10]
+    expect(getComponents(obj)).to.equal(obj)
+  }),
+    it('should return the values of the components in the object as a tuple', () => {
+      const x = 10
+      const y = 10
+      const obj = { x, y }
+      const [cx, cy] = getComponents(obj)
+      expect(cx === x && cy === y).to.be.true
+    })
 })
