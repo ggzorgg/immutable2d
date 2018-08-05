6ebe530ef647e98c6f809ab258864675318d005b
ggzorgg <41978414+ggzorgg@users.noreply.github.com>
2018-08-04 22:47:26 -0500

    Adding support for VectorLike in add

--- src/vector/add.ts
+++ src/vector/add.ts
@@ -1,5 +1,10 @@
+import { VectorLike } from '../Types'
+import { getComponents } from './Utils'
 import Vector from './Vector'
 
-export default function add({ x: x1, y: y1 }: Vector, { x: x2, y: y2 }: Vector): Vector {
+export default function add(v1: VectorLike, v2: VectorLike): Vector {
+  const [x1, y1] = getComponents(v1)
+  const [x2, y2] = getComponents(v2)
+
   return new Vector(x1 + x2, y1 + y2)
 }
--- test/vector/add.spec.ts
+++ test/vector/add.spec.ts
@@ -3,7 +3,9 @@ import 'mocha'
 import { add, toVector } from '../../src/vector'
 
 const anyNumber = fc.double(Number.MIN_VALUE, Number.MAX_VALUE)
-const anyVector = fc.tuple(anyNumber, anyNumber).map(toVector)
+const anyNumberTuple = fc.tuple(anyNumber, anyNumber)
+const anyVector = anyNumberTuple.map(toVector)
+const anyVectorObject = anyNumberTuple.map(([x, y]) => ({ x, y }))
 
 describe('The "add" function', () => {
   it('should return the component-wise sum of two vectors', () => {
@@ -17,5 +19,41 @@ describe('The "add" function', () => {
         }
       )
     )
-  })
+  }),
+    it('should return the sum of the corresponding components when called with a VectorObject', () => {
+      fc.assert(
+        fc.property(
+          anyVectorObject, anyVectorObject,
+          (v1, v2) => {
+            const v = add(v1, v2)
+
+            return v.x === v1.x + v2.x && v.y === v1.y + v2.y
+          }
+        )
+      )
+    }),
+    it('should return the sum of the corresponding components when called with tuples', () => {
+      fc.assert(
+        fc.property(
+          anyNumberTuple, anyNumberTuple,
+          (t1, t2) => {
+            const v = add(t1, t2)
+
+            return v.x === t1[0] + t2[0] && v.y === t1[1] + t2[1]
+          }
+        )
+      )
+    }),
+    it('should return the sum of the corresponding components when called with a object and a tuple', () => {
+      fc.assert(
+        fc.property(
+          anyVectorObject, anyNumberTuple,
+          (t1, t2) => {
+            const v = add(t1, t2)
+
+            return v.x === t1.x + t2[0] && v.y === t1.y + t2[1]
+          }
+        )
+      )
+    })
 })
