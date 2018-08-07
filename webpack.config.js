e5b8bba4dbc9875e8957bb100089d2ea2b769e20
ggzorgg <41978414+ggzorgg@users.noreply.github.com>
2018-08-06 20:16:18 -0500

    Adding webpack to create bundle

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: {
+    'immutable2d': './src/index.ts',
+    'immutable2d.min': './src/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'immutable2d',
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        include: /\.min\.js$/,
+      })
+    ]
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  mode: 'production'
+}
