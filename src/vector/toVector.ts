89c0684cca50a04608ba2500ed673766b5eae43c
ggzorgg <41978414+ggzorgg@users.noreply.github.com>
2018-08-11 19:27:10 -0500

    Fixing toVector export

--- src/vector/toVector.ts
+++ src/vector/toVector.ts
@@ -3,7 +3,7 @@ import { Vector } from './Vector'
 
 export function toVector(x: number, y: number): Vector
 export function toVector(values: VectorLike): Vector
-export default function toVector(x: number | VectorLike, y?: number): Vector {
+export function toVector(x: number | VectorLike, y?: number): Vector {
   if (typeof x === 'number') {
     return new Vector(x, y as number)
   } else {
