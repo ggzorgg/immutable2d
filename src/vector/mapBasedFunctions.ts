1f40c097bf7ee360d3298d245d3d5b0dd0198198
ggzorgg <41978414+ggzorgg@users.noreply.github.com>
2018-09-09 22:07:53 -0500

    Refactoring zipWith and map based operations

--- src/vector/mapBasedFunctions.ts
+++ src/vector/mapBasedFunctions.ts
@@ -2,11 +2,10 @@ import { map, mapX, mapY } from './map'
 import { VectorLike } from './types'
 
 const allMapLikeFunctions = [map, mapX, mapY]
-const createAllMapLikeFunctions = (f: (a: number) => number) => allMapLikeFunctions.map(g => g(f))
-const createAllMapLikeFunctionsWithScalar = (f: (scalar: number, a: number) => number) => allMapLikeFunctions.map(
-  // This is to ensure functions are uncurried by default
-  g => (scalar: number, v: VectorLike) => g(a => f(scalar, a))(v)
-)
+const createAllMapLikeFunctions = (f: (a: number) => number) => allMapLikeFunctions.map(m => m(f))
+const createAllMapLikeFunctionsWithScalar = (f: (scalar: number, a: number) => number) =>
+// This is to ensure functions are uncurried by default
+  allMapLikeFunctions.map(m => (scalar: number, v: VectorLike) => m(a => f(scalar, a))(v))
 
 export const [negate, negateX, negateY] = createAllMapLikeFunctions(a => -a)
 export const [multiply, multiplyX, multiplyY] = createAllMapLikeFunctionsWithScalar((scalar, a) => scalar * a)
