7a48dfb453f0ee7bca244c3378f9d1c647470203
ggzorgg <41978414+ggzorgg@users.noreply.github.com>
2018-09-10 22:32:18 -0500

    Refactoring zipWith based functions tests

--- test/vector/zipWithAssertions.ts
+++ test/vector/zipWithAssertions.ts
@@ -3,24 +3,40 @@ import { BinaryVectorAssertion } from './checkers'
 
 export type FunctionBasedBinaryVectorAssertion = (f: (a: number, b: number) => number) => BinaryVectorAssertion
 
-export const zipWithAssertion: FunctionBasedBinaryVectorAssertion = f => (v1, v2, r) => {
+const zipWithAssertion: FunctionBasedBinaryVectorAssertion = f => (v1, v2, r) => {
   expect(r.x).to.equal(f(v1.x, v2.x))
   expect(r.y).to.equal(f(v1.y, v2.y))
 }
 
-export const zipWithXAssertion: FunctionBasedBinaryVectorAssertion = f => (v1, v2, r) => {
+const zipWithXAssertion: FunctionBasedBinaryVectorAssertion = f => (v1, v2, r) => {
   expect(r.x).to.equal(f(v1.x, v2.x))
   expect(r.y).to.equal(v1.y)
 }
 
-export const zipWithYAssertion: FunctionBasedBinaryVectorAssertion = f => (v1, v2, r) => {
+const zipWithYAssertion: FunctionBasedBinaryVectorAssertion = f => (v1, v2, r) => {
   expect(r.x).to.equal(v1.x)
   expect(r.y).to.equal(f(v1.y, v2.y))
 }
 
-export type FunctionBasedBinaryVectorAssertionDefinition = [FunctionBasedBinaryVectorAssertion, string, string]
+export interface FunctionBasedBinaryVectorAssertionDefinition {
+  assertion: FunctionBasedBinaryVectorAssertion
+  predicate: string
+  suffix: string
+}
 export const allZipWithLikeAssertions: FunctionBasedBinaryVectorAssertionDefinition[] = [
-  [zipWithAssertion, 'applied component-wise', ''],
-  [zipWithXAssertion, 'applied only to the X components', 'X'],
-  [zipWithYAssertion, 'applied only to the Y components', 'Y'],
+  {
+    assertion: zipWithAssertion,
+    predicate: 'applied component-wise',
+    suffix: ''
+  },
+  {
+    assertion: zipWithXAssertion,
+    predicate: 'applied only to the X components',
+    suffix: 'X'
+  },
+  {
+    assertion: zipWithYAssertion,
+    predicate: 'applied only to the Y components',
+    suffix: 'Y'
+  }
 ]
--- test/vector/zipWithBasedFunctions.spec.ts
+++ test/vector/zipWithBasedFunctions.spec.ts
@@ -3,30 +3,38 @@ import * as ops from '../../src/vector'
 import { assertWithAllVectorKindsBinary, BinaryVectorOperation } from './checkers'
 import { allZipWithLikeAssertions, FunctionBasedBinaryVectorAssertionDefinition } from './zipWithAssertions'
 
-export type ZipWithLikeBasedFunctionDefinition = [
-  string, string,
-  (a: number, b: number) => number,
-  [BinaryVectorOperation, BinaryVectorOperation, BinaryVectorOperation]
-]
-
-const f = (a: number, b: number) => a * b
-const zipWithLikeBasedFunctions: ZipWithLikeBasedFunctionDefinition[] = [
-  ['zipWith', 'binary function aplication', f, [ops.zipWith(f), ops.zipWithX(f), ops.zipWithY(f)]],
-  ['add', 'sum', (a, b) => a + b, [ops.add, ops.addX, ops.addY]],
-  ['substract', 'difference', (a, b) => a - b, [ops.substract, ops.substractX, ops.substractY]]
-]
-
-zipWithLikeBasedFunctions.forEach(
-  ([name, action, singleFunction, zipWithLikeFunctions]) => {
-    zip(zipWithLikeFunctions, allZipWithLikeAssertions).map(
-      ([a, b]) => [a, b] as [BinaryVectorOperation, FunctionBasedBinaryVectorAssertionDefinition]
-    ).forEach(
-      ([func, [assertion, predicate, suffix]]) => {
-        describe(`The "${name}${suffix}" function`, () => {
-          assertWithAllVectorKindsBinary(
-            `should return the ${action} ${predicate} of two vectors`, func)(assertion(singleFunction))
-        })
-      }
-    )
+type BinaryNumberFunction = (a: number, b: number) => number
+const f = ((a, b) => a * b) as BinaryNumberFunction
+const zipWithLikeBasedFunctions = [
+  {
+    name: 'zipWith',
+    action: 'binary function aplication',
+    parameterFunction: f,
+    zipWithFunctions: [ops.zipWith(f), ops.zipWithX(f), ops.zipWithY(f)]
+  },
+  {
+    name: 'add',
+    action: 'sum',
+    parameterFunction: ((a, b) => a + b) as BinaryNumberFunction,
+    zipWithFunctions: [ops.add, ops.addX, ops.addY]
+  },
+  {
+    name: 'substract',
+    action: 'difference',
+    parameterFunction: ((a, b) => a - b) as BinaryNumberFunction,
+    zipWithFunctions: [ops.substract, ops.substractX, ops.substractY]
   }
-)
+]
+
+zipWithLikeBasedFunctions.forEach(definition => {
+  zip(definition.zipWithFunctions, allZipWithLikeAssertions)
+    .map(([fu, z]) => [fu, z] as [BinaryVectorOperation, FunctionBasedBinaryVectorAssertionDefinition])
+    .forEach(([func, zipWithAssertion]) => {
+      describe(`The "${definition.name}${zipWithAssertion.suffix}" function`, () => {
+        assertWithAllVectorKindsBinary(
+          `should return the ${definition.action} ${zipWithAssertion.predicate} of two vectors`,
+          func
+        )(zipWithAssertion.assertion(definition.parameterFunction))
+      })
+    })
+})
