ab8f1b0c7e31a8bab2fa0ec8ae7f792dfebcbe01
ggzorgg <41978414+ggzorgg@users.noreply.github.com>
2018-09-11 08:10:01 -0500

    Refactoring zip with based functions

--- src/vector/zipWithBasedFunctions.ts
+++ src/vector/zipWithBasedFunctions.ts
@@ -1,8 +1,8 @@
 import { zipWith, zipWithX, zipWithY } from './zipWith'
 
-const allZipWithLikeFunctions = [zipWith, zipWithX, zipWithY]
-const createAllZipWithLikeFunctions = (f: (a: number, b: number) => number) =>
-  allZipWithLikeFunctions.map(z => z(f))
+export function zipWithFunctionsFor(f: (a: number, b: number) => number) {
+  return [zipWith, zipWithX, zipWithY].map(z => z(f))
+}
 
-export const [add, addX, addY] = createAllZipWithLikeFunctions((a, b) => a + b)
-export const [substract, substractX, substractY] = createAllZipWithLikeFunctions((a, b) => a - b)
+export const [add, addX, addY] = zipWithFunctionsFor((a, b) => a + b)
+export const [substract, substractX, substractY] = zipWithFunctionsFor((a, b) => a - b)
